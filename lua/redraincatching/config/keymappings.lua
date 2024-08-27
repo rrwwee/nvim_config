fd76fc0b53853e1ba7c0054bc22d38907e2a026e
redraincatching <redraincatching@disroot.org>
2024-08-27 12:55:57 +0100

    key map function added

--- lua/redraincatching/config/keymappings.lua
+++ lua/redraincatching/config/keymappings.lua
@@ -1,4 +1,5 @@
-local keymap = vim.api.nvim_set_keymap
+local keymap = require("redraincatching.core.functions.keymap").keymap
+-- TODO: make this global
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
